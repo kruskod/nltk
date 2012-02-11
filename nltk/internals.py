kruskod/nltk

commit 79ffc65768a27784e65994628add0648993760da

new find_file method, sligtly more flexible than find_binary

nltk/internals.py
@@ -417,77 +417,78 @@ class Counter:
         return self._value
 
 ##########################################################################
-# Search for binaries
+# Search for files/binaries
 ##########################################################################
 
-def find_binary(name, path_to_bin=None, env_vars=(),
-                searchpath=(), binary_names=None, url=None,
-                verbose=True):
+def find_file(filename, env_vars=(), searchpath=(),
+        file_names=None, url=None, verbose=True):
     """
-    Search for the binary for a program that is used by nltk.
+    Search for a file to be used by nltk.
 
-    :param name: The name of the program
-    :param path_to_bin: The user-supplied binary location, or None.
+    :param name: The name of the file
+    :param path_to_file: The user-supplied binary location, or None.
     :param env_vars: A list of environment variable names to check
-    :param binary_names: A list of alternative binary names to check.
+    :param file_names: A list of alternative file names to check.
     :param searchpath: List of directories to search.
     """
-    if binary_names is None: binary_names = [name]
-    assert isinstance(name, basestring)
-    assert not isinstance(binary_names, basestring)
+    if file_names is None: file_names = [filename]
+    assert isinstance(filename, basestring)
+    assert not isinstance(file_names, basestring)
     assert not isinstance(searchpath, basestring)
     if isinstance(env_vars, basestring):
         env_vars = env_vars.split()
 
-    # If an explicit bin was given, then check it, and return it if
-    # it's present; otherwise, complain.
-    if path_to_bin is not None:
-        if os.path.isfile(path_to_bin):
-            return path_to_bin
-        for bin in binary_names:
-            if os.path.isfile(os.path.join(path_to_bin, bin)):
-                return os.path.join(path_to_bin, bin)
-            if os.path.isfile(os.path.join(path_to_bin, 'bin', bin)):
-                return os.path.join(path_to_bin, 'bin', bin)
-        raise ValueError('Could not find %s binary at %s' %
-                         (name, path_to_bin))
+    # File exists, no magic
+    if os.path.isfile(filename):
+        if verbose: print '[Found %s: %s]' % (filename, filename)
+        return filename
+    for alternative in file_names:
+        path_to_file = os.path.join(filename, alternative)
+        if os.path.isfile(path_to_file):
+            if verbose: print '[Found %s: %s]' % (filename, path_to_file)
+            return path_to_file
+        path_to_file = os.path.join(filename, 'file', alternative)
+        if os.path.isfile(path_to_file):
+            if verbose: print '[Found %s: %s]' % (filename, path_to_file)
+            return path_to_file
 
     # Check environment variables
     for env_var in env_vars:
         if env_var in os.environ:
-            path_to_bin = os.environ[env_var]
-            if os.path.isfile(path_to_bin):
-                if verbose: print '[Found %s: %s]' % (name, path_to_bin)
-                return os.environ[env_var]
+            path_to_file = os.environ[env_var]
+            if os.path.isfile(path_to_file):
+                if verbose: print '[Found %s: %s]' % (filename, path_to_file)
+                return path_to_file
             else:
-                for bin_name in binary_names:
-                    path_to_bin = os.path.join(os.environ[env_var], bin_name)
-                    if os.path.isfile(path_to_bin):
-                        if verbose: print '[Found %s: %s]'%(name, path_to_bin)
-                        return path_to_bin
-                    path_to_bin = os.path.join(os.environ[env_var], 'bin',
-                                               bin_name)
-                    if os.path.isfile(path_to_bin):
-                        if verbose: print '[Found %s: %s]'%(name, path_to_bin)
-                        return path_to_bin
+                for alternative in file_names:
+                    path_to_file = os.path.join(os.environ[env_var],
+                                                alternative)
+                    if os.path.isfile(path_to_file):
+                        if verbose: print '[Found %s: %s]'%(filename, path_to_file)
+                        return path_to_file
+                    path_to_file = os.path.join(os.environ[env_var], 'file',
+                                                alternative)
+                    if os.path.isfile(path_to_file):
+                        if verbose: print '[Found %s: %s]'%(filename, path_to_file)
+                        return path_to_file
 
     # Check the path list.
     for directory in searchpath:
-        for bin in binary_names:
-            path_to_bin = os.path.join(directory, bin)
-            if os.path.isfile(path_to_bin):
-                return path_to_bin
+        for alternative in file_names:
+            path_to_file = os.path.join(directory, alternative)
+            if os.path.isfile(path_to_file):
+                return path_to_file
 
 
     # If we're on a POSIX system, then try using the 'which' command
-    # to find the binary.
+    # to find the file.
     if os.name == 'posix':
-        for bin in binary_names:
+        for alternative in file_names:
             try:
-                p = subprocess.Popen(['which', bin], stdout=subprocess.PIPE)
+                p = subprocess.Popen(['which', alternative], stdout=subprocess.PIPE)
                 stdout, stderr = p.communicate()
                 path = stdout.strip()
-                if path.endswith(bin) and os.path.exists(path):
+                if path.endswith(alternative) and os.path.exists(path):
                     if verbose: print '[Found %s: %s]' % (name, path)
                     return path
             except KeyboardInterrupt, SystemExit:
@@ -495,12 +496,10 @@ def find_binary(name, path_to_bin=None, env_vars=(),
             except:
                 pass
 
-    msg = ("NLTK was unable to find the %s executable!  Use "
-           "config_%s()" % (name, name))
+    msg = ("NLTK was unable to find the %s file!" "\nUse software specific "
+           "configuration paramaters" % name)
     if env_vars: msg += ' or set the %s environment variable' % env_vars[0]
-    msg = textwrap.fill(msg+'.', initial_indent='  ',
-                        subsequent_indent='  ')
-    msg += "\n\n    >>> config_%s('/path/to/%s')" % (name, name)
+    msg += '.'
     if searchpath:
         msg += '\n\n  Searched in:'
         msg += ''.join('\n    - %s' % d for d in searchpath)
@@ -508,6 +507,11 @@ def find_binary(name, path_to_bin=None, env_vars=(),
                     (name, url))
     div = '='*75
     raise LookupError('\n\n%s\n%s\n%s' % (div, msg, div))
+
+def find_binary(name, path_to_bin=None, env_vars=(), searchpath=(),
+                binary_names=None, url=None, verbose=True):
+    return find_file(path_to_bin or name, env_vars, searchpath, binary_names,
+                     url, verbose)
 
 ##########################################################################
 # Find Java JAR files

nltk/tag/hunpos.py
@@ -17,7 +17,7 @@ A module for interfacing with the HunPos open-source POS-tagger.
 import os
 from subprocess import Popen, PIPE
 
-from nltk.internals import find_binary
+from nltk.internals import find_binary, find_file
 from nltk.tag.api import TaggerI
 
 _hunpos_url = 'http://code.google.com/p/hunpos/'
@@ -83,9 +83,8 @@ class HunposTagger(TaggerI):
                 url=_hunpos_url,
                 verbose=verbose)
 
-        if not os.path.isfile(path_to_model):
-            raise IOError("Hunpos model file not found: %s" % path_to_model)
-        self._hunpos_model = path_to_model
+        self._hunpos_model = find_file(path_to_model,
+                env_vars=('HUNPOS', 'HUNPOS_HOME'), verbose=verbose)
         self._encoding = encoding
         self._hunpos = Popen([self._hunpos_bin, self._hunpos_model],
                              shell=False, stdin=PIPE, stdout=PIPE, stderr=PIPE)

nltk/tag/stanford.py
@@ -17,7 +17,7 @@ import os
 import tempfile
 from subprocess import PIPE
 
-from nltk.internals import find_jar, config_java, java, _java_options
+from nltk.internals import find_file, find_jar, config_java, java, _java_options
 from nltk.tag.api import TaggerI
 
 _stanford_url = 'http://nlp.stanford.edu/software'
@@ -43,9 +43,8 @@ class StanfordTagger(TaggerI):
                 searchpath=(), url=_stanford_url,
                 verbose=verbose)
 
-        if not os.path.isfile(path_to_model):
-            raise IOError("Stanford tagger model file not found: %s" % path_to_model)
-        self._stanford_model = path_to_model
+        self._stanford_model = find_file(path_to_model,
+                env_vars=('STANFORD_MODELS'), verbose=verbose)
         self._encoding = encoding
         self.java_options = java_options
 

commit 612a221ea6047e6a420ce5392356ecf3ec81eb7f

internals: find_file: avoid NameError: use 'filename', not 'name'
See GitHub issue#217.  Problem introduced in commit 79ffc657 "new
find_file method, sligtly more flexible than find_binary".

nltk/internals.py
@@ -490,7 +490,7 @@ def find_file(filename, env_vars=(), searchpath=(),
                 stdout, stderr = p.communicate()
                 path = stdout.strip()
                 if path.endswith(alternative) and os.path.exists(path):
-                    if verbose: print '[Found %s: %s]' % (name, path)
+                    if verbose: print '[Found %s: %s]' % (filename, path)
                     return path
             except KeyboardInterrupt, SystemExit:
                 raise
@@ -498,14 +498,14 @@ def find_file(filename, env_vars=(), searchpath=(),
                 pass
 
     msg = ("NLTK was unable to find the %s file!" "\nUse software specific "
-           "configuration paramaters" % name)
+           "configuration paramaters" % filename)
     if env_vars: msg += ' or set the %s environment variable' % env_vars[0]
     msg += '.'
     if searchpath:
         msg += '\n\n  Searched in:'
         msg += ''.join('\n    - %s' % d for d in searchpath)
     if url: msg += ('\n\n  For more information, on %s, see:\n    <%s>' %
-                    (name, url))
+                    (filename, url))
     div = '='*75
     raise LookupError('\n\n%s\n%s\n%s' % (div, msg, div))
 