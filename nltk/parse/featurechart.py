b34aeb312195018215ad670af3d6dd1a1a6c069b
Denis Krusko <kruskod@gmail.com>
2015-01-13 10:35:39 +0100

    Added Performance Grammar demo function

--- nltk/parse/featurechart.py
+++ nltk/parse/featurechart.py
@@ -25,8 +25,8 @@ from nltk.parse.chart import (TreeEdge, Chart, ChartParser, EdgeI,
                               EmptyPredictRule, BottomUpPredictRule,
                               SingleEdgeFundamentalRule,
                               BottomUpPredictCombineRule,
-                              CachedTopDownPredictRule,
-                              TopDownInitRule)
+                              TopDownPredictRule,
+                              TopDownInitRule, PGLeafInitRule)
 
 #////////////////////////////////////////////////////////////
 # Tree Edge
@@ -206,7 +206,7 @@ class FeatureChart(Chart):
         for edge in self.select(start=0, end=self._num_leaves):
             if ((isinstance(edge, FeatureTreeEdge)) and
                 (edge.lhs()[TYPE] == start[TYPE]) and
-                (unify(edge.lhs(), start, rename_vars=True)) 
+                (unify(edge.lhs(), start, rename_vars=True))
                 ):
                 for tree in self.trees(edge, complete=True, tree_class=tree_class):
                     yield tree
@@ -307,53 +307,22 @@ class FeatureTopDownInitRule(TopDownInitRule):
             if chart.insert(new_edge, ()):
                 yield new_edge
 
-class FeatureTopDownPredictRule(CachedTopDownPredictRule):
+class FeatureTopDownPredictRule(TopDownPredictRule):
     """
-    A specialized version of the (cached) top down predict rule that operates
-    on nonterminals whose symbols are ``FeatStructNonterminal``s.  Rather
-    than simply comparing the nonterminals for equality, they are
-    unified.
+    A rule licensing edges corresponding to the grammar productions
+    for the nonterminal following an incomplete edge's dot.  In
+    particular, this rule specifies that
+    ``[A -> alpha \* B beta][i:j]`` licenses the edge
+    ``[B -> \* gamma][j:j]`` for each grammar production ``B -> gamma``.
 
-    The top down expand rule states that:
-
-    - ``[A -> alpha \* B1 beta][i:j]``
-
-    licenses the edge:
-
-    - ``[B2 -> \* gamma][j:j]``
-
-    for each grammar production ``B2 -> gamma``, assuming that B1
-    and B2 can be unified.
+    :note: This rule corresponds to the Predictor Rule in Earley parsing.
     """
     def apply(self, chart, grammar, edge):
         if edge.is_complete(): return
-        nextsym, index = edge.nextsym(), edge.end()
-        if not is_nonterminal(nextsym): return
-
-        # If we've already applied this rule to an edge with the same
-        # next & end, and the chart & grammar have not changed, then
-        # just return (no new edges to add).
-        done = self._done.get((nextsym, index), (None,None))
-        if done[0] is chart and done[1] is grammar: return
-
         for prod in grammar.productions(lhs=edge.nextsym()):
-            # If the left corner in the predicted production is
-            # leaf, it must match with the input.
-            if prod.rhs():
-                first = prod.rhs()[0]
-                if is_terminal(first):
-                    if index >= chart.num_leaves(): continue
-                    if first != chart.leaf(index): continue
-
-            # We rename vars here, because we don't want variables
-            # from the two different productions to match.
-            if unify(prod.lhs(), edge.next_with_bindings(), rename_vars=True):
-                new_edge = FeatureTreeEdge.from_production(prod, edge.end())
-                if chart.insert(new_edge, ()):
-                    yield new_edge
-
-        # Record the fact that we've applied this rule.
-        self._done[nextsym, index] = (chart, grammar)
+            new_edge = FeatureTreeEdge.from_production(prod, edge.end())
+            if chart.insert(new_edge, ()):
+                yield new_edge
 
 
 #////////////////////////////////////////////////////////////
@@ -563,20 +532,21 @@ def run_profile():
     p.strip_dirs().sort_stats('time', 'cum').print_stats(60)
     p.strip_dirs().sort_stats('cum', 'time').print_stats(60)
 
-if __name__ == '__main__':
+def pg_demo():
+    """
+    Demo for Performance Grammar FeatureChart
+    """
     from nltk.data import load
-    #demo()
-    #print()
-
-    import os
-    os.chdir(os.path.dirname(__file__))
-    print(os.getcwd())
-
-    sent = 'ich sehe den Mann'
+    import itertools
+    sent = 'ich sehe den Mann mit dem Hund'
     grammar = load('../../examples/grammars/book_grammars/pg_german.fcfg')
-    #sent = 'Kim likes children'
-    cp = FeatureTopDownChartParser(grammar, trace=2)
+
     tokens = sent.split()
-    trees = cp.parse(tokens)
-    for tree in trees:
-        print(tree)
+    for comb in itertools.permutations(tokens):
+        cp = FeatureTopDownChartParser(grammar, trace=0)
+        trees = cp.parse(comb)
+        for tree in trees:
+            print(tree)
+
+if __name__ == '__main__': pg_demo()
+
